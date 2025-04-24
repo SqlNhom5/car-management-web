3cf77ba9ca1a863fdd31b001ab0c765d7acc58b6
Hoàng Khánh Văn <vankhanh3704@gmail.com>
2025-04-24 10:55:18 +0700

    filter api

--- backend/src/main/java/com/vehicle/marketplace/configuration/SecurityConfig.java
+++ backend/src/main/java/com/vehicle/marketplace/configuration/SecurityConfig.java
@@ -1,12 +1,52 @@
 package com.vehicle.marketplace.configuration;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableMethodSecurity
+@EnableWebSecurity
 public class SecurityConfig {
+    @Autowired
+    private CustomJwtDecoder customJwtDecoder;
+
+    private final String[] PUBLIC_ENDPOINTS = {
+            "/auth/token"
+    };
+
+
+    // Security
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.authorizeHttpRequests(authorizeRequests -> authorizeRequests
+                .requestMatchers(HttpMethod.POST, PUBLIC_ENDPOINTS).permitAll()
+                .anyRequest().authenticated()
+        );
+
+        // xử lý authenticated : sau khi login phải dùng token để truy cập
+        httpSecurity.oauth2ResourceServer(
+                oauth2 -> oauth2
+                        .jwt(jwtConfigurer -> jwtConfigurer
+                                .decoder(customJwtDecoder)
+                        )
+
+        );
+
+        httpSecurity.csrf(AbstractHttpConfigurer::disable);
+        return httpSecurity.build();
+    }
+
+
+    // mã hoá password
     @Bean
     PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder(10);
