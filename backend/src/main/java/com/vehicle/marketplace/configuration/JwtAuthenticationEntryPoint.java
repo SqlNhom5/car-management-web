6da9a3ea20f6465824bff70fd2577d2bda6888c2
Hoàng Khánh Văn <vankhanh3704@gmail.com>
2025-04-25 11:49:30 +0700

    xử lý lỗi trả về khi không authentication

---
+++ backend/src/main/java/com/vehicle/marketplace/configuration/JwtAuthenticationEntryPoint.java
@@ -0,0 +1,35 @@
+package com.vehicle.marketplace.configuration;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.vehicle.marketplace.Enum.ErrorCode;
+import com.vehicle.marketplace.model.response.ApiResponse;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+
+import java.io.IOException;
+
+public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    // xảy ra khi mà có 1 exception mà authentication không thành công
+    @Override
+    public void commence(
+            HttpServletRequest request, HttpServletResponse response, AuthenticationException authException)
+            throws IOException, ServletException {
+        ErrorCode errorCode = ErrorCode.UNAUTHENTICATED;
+        response.setStatus(errorCode.getHttpStatusCode().value());
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+
+        ApiResponse<?> apiResponse = ApiResponse.builder()
+                .code(errorCode.getCode())
+                .message(errorCode.getMessage())
+                .build();
+
+        ObjectMapper mapper = new ObjectMapper();
+        response.getWriter().write(mapper.writeValueAsString(apiResponse));
+        response.flushBuffer();
+    }
+}
--- backend/src/main/java/com/vehicle/marketplace/configuration/SecurityConfig.java
+++ backend/src/main/java/com/vehicle/marketplace/configuration/SecurityConfig.java
@@ -38,7 +38,8 @@ public class SecurityConfig {
         httpSecurity.oauth2ResourceServer(oauth2 -> oauth2
                 .jwt(jwtConfigurer -> jwtConfigurer
                         .decoder(customJwtDecoder)
-                        .jwtAuthenticationConverter(jwtAuthenticationConverter()))
+                        .jwtAuthenticationConverter(jwtAuthenticationConverter())
+                ).authenticationEntryPoint(new JwtAuthenticationEntryPoint())
         );
 
         httpSecurity.csrf(AbstractHttpConfigurer::disable);
