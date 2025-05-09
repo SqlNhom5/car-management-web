1011c6e3e1db01a4b29274af912dead8c5710f9f
Hoàng Khánh Văn <vankhanh3704@gmail.com>
2025-05-09 19:30:28 +0700

    target

---
+++ backend/target/generated-sources/annotations/com/vehicle/marketplace/mapper/CarMapperImpl.java
@@ -0,0 +1,97 @@
+package com.vehicle.marketplace.mapper;
+
+import com.vehicle.marketplace.Entity.CarEntity;
+import com.vehicle.marketplace.model.request.CarCreationRequest;
+import com.vehicle.marketplace.model.request.CarUpdateRequest;
+import com.vehicle.marketplace.model.response.CarResponse;
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    comments = "version: 1.5.5.Final, compiler: javac, environment: Java 17.0.10 (Oracle Corporation)"
+)
+@Component
+public class CarMapperImpl implements CarMapper {
+
+    @Override
+    public CarEntity toCarEntity(CarCreationRequest carCreationRequest) {
+        if ( carCreationRequest == null ) {
+            return null;
+        }
+
+        CarEntity.CarEntityBuilder carEntity = CarEntity.builder();
+
+        carEntity.carName( carCreationRequest.getCarName() );
+        carEntity.brand( carCreationRequest.getBrand() );
+        carEntity.model( carCreationRequest.getModel() );
+        carEntity.manufactureYear( carCreationRequest.getManufactureYear() );
+        carEntity.licensePlate( carCreationRequest.getLicensePlate() );
+        carEntity.price( carCreationRequest.getPrice() );
+        carEntity.count( carCreationRequest.getCount() );
+        carEntity.status( carCreationRequest.getStatus() );
+        carEntity.color( carCreationRequest.getColor() );
+        carEntity.specifications( carCreationRequest.getSpecifications() );
+        carEntity.imageUrl( carCreationRequest.getImageUrl() );
+        carEntity.warrantyPeriod( carCreationRequest.getWarrantyPeriod() );
+        carEntity.numberOfSeats( carCreationRequest.getNumberOfSeats() );
+        carEntity.fuel( carCreationRequest.getFuel() );
+        carEntity.gear( carCreationRequest.getGear() );
+        carEntity.note( carCreationRequest.getNote() );
+
+        return carEntity.build();
+    }
+
+    @Override
+    public CarResponse toCarResponse(CarEntity carEntity) {
+        if ( carEntity == null ) {
+            return null;
+        }
+
+        CarResponse carResponse = new CarResponse();
+
+        carResponse.setCarId( carEntity.getCarId() );
+        carResponse.setCarName( carEntity.getCarName() );
+        carResponse.setBrand( carEntity.getBrand() );
+        carResponse.setModel( carEntity.getModel() );
+        carResponse.setManufactureYear( carEntity.getManufactureYear() );
+        carResponse.setLicensePlate( carEntity.getLicensePlate() );
+        carResponse.setPrice( carEntity.getPrice() );
+        carResponse.setCount( carEntity.getCount() );
+        carResponse.setStatus( carEntity.getStatus() );
+        carResponse.setColor( carEntity.getColor() );
+        carResponse.setSpecifications( carEntity.getSpecifications() );
+        carResponse.setImageUrl( carEntity.getImageUrl() );
+        carResponse.setWarrantyPeriod( carEntity.getWarrantyPeriod() );
+        carResponse.setNumberOfSeats( carEntity.getNumberOfSeats() );
+        carResponse.setFuel( carEntity.getFuel() );
+        carResponse.setGear( carEntity.getGear() );
+        carResponse.setNote( carEntity.getNote() );
+
+        return carResponse;
+    }
+
+    @Override
+    public void updateCar(CarEntity carEntity, CarUpdateRequest request) {
+        if ( request == null ) {
+            return;
+        }
+
+        carEntity.setCarName( request.getCarName() );
+        carEntity.setBrand( request.getBrand() );
+        carEntity.setModel( request.getModel() );
+        carEntity.setManufactureYear( request.getManufactureYear() );
+        carEntity.setLicensePlate( request.getLicensePlate() );
+        carEntity.setPrice( request.getPrice() );
+        carEntity.setCount( request.getCount() );
+        carEntity.setStatus( request.getStatus() );
+        carEntity.setColor( request.getColor() );
+        carEntity.setSpecifications( request.getSpecifications() );
+        carEntity.setImageUrl( request.getImageUrl() );
+        carEntity.setWarrantyPeriod( request.getWarrantyPeriod() );
+        carEntity.setNumberOfSeats( request.getNumberOfSeats() );
+        carEntity.setFuel( request.getFuel() );
+        carEntity.setGear( request.getGear() );
+        carEntity.setNote( request.getNote() );
+    }
+}
---
+++ backend/target/generated-sources/annotations/com/vehicle/marketplace/mapper/UserMapperImpl.java
@@ -0,0 +1,95 @@
+package com.vehicle.marketplace.mapper;
+
+import com.vehicle.marketplace.Entity.RoleEntity;
+import com.vehicle.marketplace.Entity.UserEntity;
+import com.vehicle.marketplace.model.request.UserCreationRequest;
+import com.vehicle.marketplace.model.request.UserUpdateRequest;
+import com.vehicle.marketplace.model.response.RoleResponse;
+import com.vehicle.marketplace.model.response.UserResponse;
+import java.util.LinkedHashSet;
+import java.util.Set;
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    comments = "version: 1.5.5.Final, compiler: javac, environment: Java 17.0.10 (Oracle Corporation)"
+)
+@Component
+public class UserMapperImpl implements UserMapper {
+
+    @Override
+    public UserEntity toUser(UserCreationRequest userCreationRequest) {
+        if ( userCreationRequest == null ) {
+            return null;
+        }
+
+        UserEntity.UserEntityBuilder userEntity = UserEntity.builder();
+
+        userEntity.username( userCreationRequest.getUsername() );
+        userEntity.password( userCreationRequest.getPassword() );
+        userEntity.firstName( userCreationRequest.getFirstName() );
+        userEntity.lastName( userCreationRequest.getLastName() );
+        userEntity.email( userCreationRequest.getEmail() );
+        userEntity.phone( userCreationRequest.getPhone() );
+
+        return userEntity.build();
+    }
+
+    @Override
+    public UserResponse toUserResponse(UserEntity userEntity) {
+        if ( userEntity == null ) {
+            return null;
+        }
+
+        UserResponse userResponse = new UserResponse();
+
+        userResponse.setRoles( roleEntitySetToRoleResponseSet( userEntity.getRoles() ) );
+        userResponse.setId( userEntity.getId() );
+        userResponse.setUsername( userEntity.getUsername() );
+        userResponse.setFirstName( userEntity.getFirstName() );
+        userResponse.setLastName( userEntity.getLastName() );
+        userResponse.setEmail( userEntity.getEmail() );
+        userResponse.setPhone( userEntity.getPhone() );
+
+        return userResponse;
+    }
+
+    @Override
+    public void updateUser(UserEntity userEntity, UserUpdateRequest request) {
+        if ( request == null ) {
+            return;
+        }
+
+        userEntity.setPassword( request.getPassword() );
+        userEntity.setFirstName( request.getFirstName() );
+        userEntity.setLastName( request.getLastName() );
+        userEntity.setPhone( request.getPhone() );
+    }
+
+    protected RoleResponse roleEntityToRoleResponse(RoleEntity roleEntity) {
+        if ( roleEntity == null ) {
+            return null;
+        }
+
+        RoleResponse.RoleResponseBuilder roleResponse = RoleResponse.builder();
+
+        roleResponse.name( roleEntity.getName() );
+        roleResponse.description( roleEntity.getDescription() );
+
+        return roleResponse.build();
+    }
+
+    protected Set<RoleResponse> roleEntitySetToRoleResponseSet(Set<RoleEntity> set) {
+        if ( set == null ) {
+            return null;
+        }
+
+        Set<RoleResponse> set1 = new LinkedHashSet<RoleResponse>( Math.max( (int) ( set.size() / .75f ) + 1, 16 ) );
+        for ( RoleEntity roleEntity : set ) {
+            set1.add( roleEntityToRoleResponse( roleEntity ) );
+        }
+
+        return set1;
+    }
+}
